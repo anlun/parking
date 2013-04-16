2bda169f4ce61f6c08c0288fa9552681c2c3997a
Podkopaev Anton <podkoav239@gmail.com>
2013-04-16 23:20:03 +0400

    Added parsing

--- datareader.py
+++ datareader.py
@@ -2,19 +2,42 @@
 # vim: set fileencoding=utf-8 :
 import urllib
 import os
+import MySQLdb
+import csv
 
 # 497 - номер opendata c перехватывающими парковками
-data_number     = "497"
-archive_address = "http://gis-lab.info/data/mos.ru/%s.7z" % data_number
-archive_name    = "data.7z"
+data_number     = '497'
+archive_address = 'http://gis-lab.info/data/mos.ru/%s.7z' % data_number
+archive_name    = 'data.7z'
 
 # Скачать архив
 urllib.urlretrieve (archive_address, archive_name)
 
 # Разархивировать
-os.system("7z x %s " % archive_name)
-os.system("mv %s.csv data.csv" % data_number)
+os.system('7z x %s ' % archive_name)
+os.system('mv %s.csv data.csv' % data_number)
 
 # Распарсить csv
+with open('data.csv', 'rb') as csvfile:
+	parkreader = csv.reader(csvfile, delimiter=';')
+	flag = 0
+	for row in parkreader:
+		if flag == 0: # не обрабатывать первую строчку
+			flag = 1
+			continue
+
+		if flag > 5: # just for tests
+			break
+
+		fields = [x.decode('cp1251').encode('utf-8') for x in row]
+
+		uid     = fields[1]
+		address = fields[4]
+		x_pos   = fields[5]
+		y_pos   = fields[6]
+		size    = fields[13]
+
+		print ', '.join([uid, address, x_pos, y_pos, size])
+		flag += 1
 
 # Записать в БД
